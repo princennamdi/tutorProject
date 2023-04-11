c94342063e265a49580923bdb19b9f32d66ece5b
nwene <nwenedokus@gmail.com>
2023-04-11 09:35:39 +0100

    done IO

--- inputOutput/src/fileIO.java
+++ inputOutput/src/fileIO.java
@@ -1,2 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class fileIO {
+
+    public static void main(String[] args){
+        File fl;
+        Scanner sc;
+        String line;
+
+        fl = new File("C:\\Users\\new\\Documents\\gym.exe");
+
+        try {
+            sc = new Scanner(fl);
+
+            while(sc.hasNextLine()){
+                line = sc.nextLine();
+                System.out.println(line);
+
+            }
+            sc.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
 }
